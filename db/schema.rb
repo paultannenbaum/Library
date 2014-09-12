39bcd8dd167c6494fb0a14bdbba490573ba55016
Paul Tannenbaum <paultannenbaum@gmail.com>
2014-09-11 23:24:18 -0700

    created chapter resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140912061126) do
+ActiveRecord::Schema.define(version: 20140912062332) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20140912061126) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "genre_id"
+  end
+
+  create_table "chapters", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "genres", force: true do |t|
