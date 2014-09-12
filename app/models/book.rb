af8b3f7f51fa4264bd63c63eb68f361fb0cf18f2
Paul Tannenbaum <paultannenbaum@gmail.com>
2014-09-11 23:27:04 -0700

    associated books and chapters

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ActiveRecord::Base
   belongs_to :genre
+  has_many   :chapters
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140912062332) do
+ActiveRecord::Schema.define(version: 20140912062511) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140912062332) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "book_id"
   end
 
   create_table "genres", force: true do |t|
