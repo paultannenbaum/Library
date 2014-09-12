a78311ad34e65ac3290a18ed62c28420c55f1e29
Paul Tannenbaum <paultannenbaum@gmail.com>
2014-09-11 23:55:58 -0700

    created basic seed data

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ActiveRecord::Base
   belongs_to :genre
-  has_many   :chapters
+  has_many   :chapters, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+genres = Genre.create([
+    { name: 'Horror'      },
+    { name: 'Sci-Fi'      },
+    { name: 'Science'     },
+    { name: 'Biographies' },
+    { name: 'History'     },
+    { name: 'Fantasy'     },
+    { name: 'Health'      }])
+
+books = Genre.all.each do | g |
+  10.times do | i |
+    Book.create(
+      title: "#{g.name}: Book number #{i.to_s}",
+      author: "John Doe number #{i.to_s}",
+      genre_id: g.id
+    )
+  end
+end
+
+chapters = Book.all.each do | b |
+  10.times do | i |
+    Chapter.create(
+        title: "#{b.title}: Chapter #{i.to_s}",
+        book_id: b.id
+    )
+  end
+end
